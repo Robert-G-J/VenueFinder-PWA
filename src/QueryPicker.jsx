96b2d6d8ac45a02ce4489ef57c9a36acb0ec6586
Robert Jones <robert.g.jones@gmail.com>
2018-01-26 12:44:03 +0000

    Add simple skeleton of query picker

---
+++ src/QueryPicker.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+const QueryPicker = () => (
+  <div className="picker">
+    <ul className="picker__list">
+      <li className="picket__item">
+        <button onClick={alert("krang!")} />
+      </li>
+    </ul>
+  </div>
+);
+
+export default QueryPicker;
