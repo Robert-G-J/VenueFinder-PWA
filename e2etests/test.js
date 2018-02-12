1d1c5ff6366c6e9eecfa6e66c381b0848f1e8c82
Robert Jones <robert.g.jones@gmail.com>
2018-02-12 16:11:58 +0000

    Write (failing) e2e that expects a list

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const expect = require("chai").expect;
 
 describe("Location searching", () => {
@@ -6,5 +7,17 @@ describe("Location searching", () => {
     const actualTitle = browser.getTitle();
 
     expect(actualTitle).to.eql("Search Spots");
+  });
+
+  it("Should allow me to search by current location", () => {
+    const spotText = "coffee";
+    browser.url("http://localhost:3000/");
+    browser
+      .click(".search-location__button")
+      .element(".search-bar__input")
+      .setValue(spotText)
+      .click(".search-bar__button");
+    const list = browser.element(".spot-table__list").getText();
+    expect(list).to.eq("Friska");
   });
 });
