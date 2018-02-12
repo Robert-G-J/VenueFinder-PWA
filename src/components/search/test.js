a78f6902ff3c02691c6686a809c3d895b9fb07d2
Robert Jones <robert.g.jones@gmail.com>
2018-02-12 17:49:42 +0000

    Update Search app test

--- src/components/search/test.js
+++ src/components/search/test.js
@@ -1,16 +1,16 @@
+/* eslint-disable */
+
 import React from "react";
 import Search from ".";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
 
-describe("The Search component", () => {
-  it("renders correctly", () => {
-    const tree = renderer.create(<Search />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it("Search app renders correctly", () => {
+  const tree = renderer.create(<Search />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
 
-  it("renders without crashing", () => {
-    const component = shallow(<Search />);
-    expect(component.exists()).toEqual(true);
-  });
+it("Search app doesn't crash", () => {
+  const component = shallow(<Search />);
+  expect(component.exists()).toEqual(true);
 });
