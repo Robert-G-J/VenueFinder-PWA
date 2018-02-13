a133dc04efd162c66e90014ffab5400eeea08005
Robert Jones <robert.g.jones@gmail.com>
2018-02-13 18:02:28 +0000

    Add redux-thunk for async action creators

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import thunk from "redux-thunk";
 import Search from "./components/search/";
 import registerServiceWorker from "./registerServiceWorker";
+
+import "./index.css";
 
 ReactDOM.render(<Search />, document.getElementById("root"));
 registerServiceWorker();
