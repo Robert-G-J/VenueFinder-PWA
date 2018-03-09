617057743af3a1696b2ca9f54a5a463d658b8420
Robert Jones <robert.g.jones@gmail.com>
2018-03-09 14:54:24 +0000

    Obscure keys using .env.local file
    Previous key had lapsed after accidental commital

--- src/services/fsqAPI.js
+++ src/services/fsqAPI.js
@@ -1,8 +1,6 @@
-import object from "lodash/object";
-
 const foursquare = require("react-foursquare")({
-  clientID: "A150E11L5JS0WTGP0C1F5E4HAJHKDICLYKQIQTDGX4K21LLE",
-  clientSecret: "UEYU0RJNPXDXLDY5FNF1DTSZJ4HO3CUEBTNHTJWDJNB1WLPX"
+  clientID: process.env.REACT_APP_FSQ_CLIENT_ID,
+  clientSecret: process.env.REACT_APP_FSQ_CLIENT_PRIVATE_KEY
 });
 
 export const suggestCompletion = params =>
