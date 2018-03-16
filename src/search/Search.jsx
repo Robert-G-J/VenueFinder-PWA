3523c1c41da78ab99989df175663abe4b8469ab6
Robert Jones <robert.g.jones@gmail.com>
2018-03-16 15:58:29 +0000

    Improve styling with flex-box

--- src/search/Search.jsx
+++ src/search/Search.jsx
@@ -4,12 +4,15 @@ import SpotListContainer from "../spot-list/SpotListContainer";
 import "./index.css";
 
 const Search = () => (
-  <div className="search__body">
+  <div className="search__container">
     <header className="search__header">
       <h1>Search</h1>
       <SearchBarContainer />
     </header>
-    <SpotListContainer />
+    <section>
+      <SpotListContainer />
+    </section>
+    <footer className="search__footer">Footer</footer>
   </div>
 );
 export default Search;
