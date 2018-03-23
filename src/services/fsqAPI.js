26f11be071eca4eaa7e26472bebcd354daf35936
Robert Jones <robert.g.jones@gmail.com>
2018-03-23 17:59:12 +0000

    Configure for build:

--- src/services/fsqAPI.js
+++ src/services/fsqAPI.js
@@ -1,4 +1,4 @@
-const foursquare = require("react-foursquare")({
+const foursquare = require("react-foursquare-es5-mod")({
   clientID: process.env.REACT_APP_FSQ_CLIENT_ID,
   clientSecret: process.env.REACT_APP_FSQ_CLIENT_PRIVATE_KEY
 });
